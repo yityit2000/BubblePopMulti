d143731a5d9b4f704079a2b7cc1ca4f10f056455
Etai Dvora <dvora.etai@gmail.com>
2018-12-21 02:38:01 -0500

    Added scoring and starting powerups

---
+++ BubblePopShared/Code/Powerup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BubblePop
+{
+    abstract class Powerup : Bubble
+    {
+        public Powerup(Vector2 position, Color bubbleColor, Texture2D bubbleTexture) : base(position, bubbleColor, bubbleTexture)
+        {
+        }
+
+
+    }
+}
--- BubblePopShared/Code/Score.cs
+++ BubblePopShared/Code/Score.cs
@@ -24,9 +24,20 @@ namespace BubblePop
             gameScore += ScoreOfCurrentBatch(numberOfClearedBubbles, level);
         }
 
+        // This integer houses the current scoring algorithm used. Basically every connected bubble in a batch gives one
+        // more point than the previous. So 2 bubbles give 3 points (1 + 2), 3 give 6 (1 + 2 + 3), 4 give 10 (1 + 2 + 3 + 4),
+        // and so on. We then multiply by a scoring unit in case we want to change the base score for a bubble, and then
+        // multiply by the level, awarding more points for the same amount of bubbles in later levels since it's more
+        // difficult to get larger groups (for now. No powerups yet). This is likely temporary.
         public int ScoreOfCurrentBatch(int numberInBatch, int level)
         {
             int scoreToCalculate = 0;
+
+            if (numberInBatch < 2)
+            {
+                return scoreToCalculate;
+            }
+
             for (int i = 1; i < numberInBatch + 1; i++)
             {
                 scoreToCalculate += i * Constants.SCORING_UNIT * level;
